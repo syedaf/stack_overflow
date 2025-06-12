6f3fe5d8ac0511877f75245cbfc15d452400ada3
Syed <sygaki786@gmail.com>
2025-06-12 05:43:45 -0400

    Added fonts. Config changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => {
         return (
                 <div>
-                        <h1 className="text-3xl text-light-500">test</h1>
+                        <h1 className="h1-bold">NextJS 15</h1>
+                        <h1 className="h1-bold font-inter">
+                                NextJS 15 - Inter
+                        </h1>
+                        <h1 className="h1-bold font-space-grotesk">
+                                NextJS 15 - Grotesk
+                        </h1>
                 </div>
         );
 };
